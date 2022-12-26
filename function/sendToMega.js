7769457da6178fc03c6d6e8426dfa46719cfeaaf
Your Name <you@example.com>
2022-12-26 14:12:53 +0000

    log checking

--- function/sendToMega.js
+++ function/sendToMega.js
@@ -5,13 +5,13 @@ const sendToMega = async (req, res) => {
   try {
     console.log("called send-to-mega");
     const file = req.file;
-    console.log(file);
+    console.log(req);
     const storage = await new Storage({
       email: process.env.MEGA_EMAIL,
       password: process.env.MEGA_PASSWORD,
     }).ready;
     console.log("logged in");
-    const data = fs.readFileSync(`/tmp/${file.filename}`);
+    // const data = fs.readFileSync(`/tmp/${file.filename}`);
 
     // const upFile = await storage.upload(file.originalname, data).complete;
     req.pip(await storage.upload(file.originalname, data).complete);
