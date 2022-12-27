a9ba89fff562c4856cc0acba1668eb754fecc12a
Your Name <you@example.com>
2022-12-27 05:37:12 +0000

    size

--- function/sendToMega.js
+++ function/sendToMega.js
@@ -4,25 +4,25 @@ const { Storage } = require("megajs");
 const sendToMega = async (req, res) => {
   try {
     console.log("called send-to-mega");
-    const file = req.files.file;
-    console.log(req.files.file);
+    // const file = req.files.file;
+    // console.log(req);
     const storage = await new Storage({
       email: process.env.MEGA_EMAIL,
       password: process.env.MEGA_PASSWORD,
     }).ready;
     console.log("logged in");
-
-    const fileStream = fs.createReadStream(file.path);
-    const uploadStream = storage.upload({
-      name: file.originalname,
-      size: file.size,
-    });
-    fileStream.pipe(uploadStream);
-
-    const uploadFile = await fileStream.complete;
+    // const data = fs.readFileSync(`/tmp/${file.filename}`);
+    // const fileStream = fs.createReadStream('hello-world.txt')
 
     // const upFile = await storage.upload(file.originalname, data).complete;
-    // fileStream.pip(await storage.upload("test.txt", file).complete);
+    // req.pip(await storage.upload("test.txt", file).complete);
+    const uploadStream = storage.upload({
+      name: "hello-world.txt",
+      size: "17mb",
+    });
+    req.pipe(uploadStream);
+
+    const uploadFile = await fileStream.complete;
     // fs.unlinkSync(file.path);
     console.log("success");
     res.status(200).json({ message: "success" });
