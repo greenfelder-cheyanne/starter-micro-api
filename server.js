828f373d394f8db9effe3929de90c5a6a29a30e4
Your Name <you@example.com>
2022-12-27 07:02:29 +0000

    code revert

--- server.js
+++ server.js
@@ -2,16 +2,20 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+const multer = require("multer");
 
 const port = process.env.PORT || 5000;
 
 const app = express();
 app.use(cors());
 app.use(express.json({ extended: true, limit: "500mb" }));
-// app.use(bodyParser.urlencoded({ extended: true, limit: "500mb" }));
-// const upload = multer({ dest: "/tmp/", limits: { fileSize: "500mb" } });
+const upload = multer({ dest: "/tmp/", limits: { fileSize: "500mb" } });
 
-app.use("/file-upload", require("./function/sendToMega"));
+app.use(
+  "/file-upload",
+  upload.single("file"),
+  require("./function/sendToMega")
+);
 
 app.use(express.static(path.join(__dirname, "/public")));
 app.use("*", (req, res) => {
