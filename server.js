86b7eafabdddb714dd9eac8b39b38712cbce5f77
Your Name <you@example.com>
2023-01-03 16:59:04 +0000

    added try catch

--- server.js
+++ server.js
@@ -1,35 +1,39 @@
 module.exports = () => {
-  const express = require("express");
-  const fs = require("fs");
-  const cors = require("cors");
-  const http = require("http");
+  try {
+    const express = require("express");
+    const fs = require("fs");
+    const cors = require("cors");
+    const http = require("http");
 
-  const socket = require("./socket");
+    const socket = require("./socket");
 
-  const app = express();
-  global.rootDir = __dirname;
-  const server = http.createServer(app);
-  let port = 5001;
+    const app = express();
+    global.rootDir = __dirname;
+    const server = http.createServer(app);
+    let port = 5001;
 
-  app.use(cors());
-  app.use(express.json({ extended: false, limit: "250mb" }));
-  // app.use(
-  //   express.urlencoded({
-  //     limit: "250mb",
-  //     extended: true,
-  //     parameterLimit: 50000,
-  //   })
-  // );
-  // app.use(express.static("uploads"));
-  // app.use("/uploads", express.static("public"));
-  app.use("/chat", require("./api"));
-  app.get("*", (req, res) => {
-    res.send("Welcome to Chat ver: 2.0.0");
-  });
+    app.use(cors());
+    app.use(express.json({ extended: false, limit: "250mb" }));
+    // app.use(
+    //   express.urlencoded({
+    //     limit: "250mb",
+    //     extended: true,
+    //     parameterLimit: 50000,
+    //   })
+    // );
+    // app.use(express.static("uploads"));
+    // app.use("/uploads", express.static("public"));
+    app.use("/chat", require("./api"));
+    app.get("*", (req, res) => {
+      res.send("Welcome to Chat ver: 2.0.0");
+    });
 
-  socket(server, port);
+    socket(server, port);
 
-  server.listen(port, () => {
-    console.log("server running on port: " + port);
-  });
+    server.listen(port, () => {
+      console.log("server running on port: " + port);
+    });
+  } catch (e) {
+    console.log(e);
+  }
 };
