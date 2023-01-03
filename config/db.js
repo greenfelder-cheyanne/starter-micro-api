f1835f97b3ce3f2673228f7bf00a675c2cfa7af6
Your Name <you@example.com>
2023-01-03 15:13:44 +0000

    added log

--- config/db.js
+++ config/db.js
@@ -1,6 +1,7 @@
 const mongoose = require("mongoose");
 
 mongoose.set("strictQuery", false);
+console.log("<config-db>DB_URL: ", process.env.DB_URL);
 const connection = mongoose.connect(process.env.DB_URL, {
   useNewUrlParser: true,
   useUnifiedTopology: true,
